467f5d79941242fcbca7db27f1837ed34aecf9ab
Eric <ebeecroft@mail.csuchico.edu>
2015-04-15 21:37:46 -0700

    Removed the old RC 6.0 files

--- SonarBat/SonarBat/SonarBat RC 6.0/src/sonar/gamestates/states/levels/LevelHolder.java
+++
@@ -1,9 +0,0 @@
-package sonar.gamestates.states.levels;
-
-class LevelHolder
-{
-	//Holds all the levels in the actual game.
-	static final int starterStage1 = 0;
-	static final int starterStage2 = 1;
-	static final int starterStage3 = 2;
-}
