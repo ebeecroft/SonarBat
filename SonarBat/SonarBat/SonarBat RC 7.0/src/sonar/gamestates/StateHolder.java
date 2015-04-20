e0a88735b6f9df3ac76b9133838b2f39fe39d2e1
Eric <ebeecroft@mail.csuchico.edu>
2015-04-19 20:12:43 -0700

    Removed the old RC 7.0 files

--- SonarBat/SonarBat/SonarBat RC 7.0/src/sonar/gamestates/StateHolder.java
+++
@@ -1,9 +0,0 @@
-package sonar.gamestates;
-
-public class StateHolder
-{
-	//Holds all of the states that are currently available in the game.
-	static final int menuState = 0;
-	public static final int passwordState = 1;
-	public static final int starterStage = 2;
-}
